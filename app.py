ea8c2b156ac84941c2b3598524d3727782a4b46f
myname21-meet <myname21@meet.mit.edu>
2019-12-12 11:42:11 -0500

    products are in my website

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/home page')
+@app.route('/')
 def home():
-    return "home page"
-@app.route('/student/<int:student_id>') 
+    return render_template("home.html")
+
+
+@app.route('home') 
 def student_id(student_id):
 	return render_template(
 		"student.html", student=query_by_id(student_id))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
